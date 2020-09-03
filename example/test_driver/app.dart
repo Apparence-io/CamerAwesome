bb35ddeab539b322b0803d1ff9102f8641ddea4d
Gautier <gautier@apparence.io>
2020-09-03 15:12:20 +0200

    + next

--- example/test_driver/app.dart
+++ example/test_driver/app.dart
@@ -65,8 +65,9 @@ void main() {
     // change photo size preset
     var previousResolution = (find.byKey(ValueKey("resolutionTxt")).evaluate().first.widget as Text).data;
     await tester.tap(find.byKey(ValueKey("resolutionButton")));
+    await tester.pump(Duration(milliseconds: 1000));
     await tester.pumpAndSettle(Duration(milliseconds: 1000));
-    await tester.tap(find.byKey(ValueKey("resOption")).last);
+    await tester.tap(find.byKey(ValueKey("resOption")).at(1));
     await tester.pump(Duration(milliseconds: 500));
     var currentResolution = (find.byKey(ValueKey("resolutionTxt")).evaluate().first.widget as Text).data;
     expect(previousResolution, isNot(equals(currentResolution)));
