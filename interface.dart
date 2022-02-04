fb7ce1205c83855497bfa2f72efe7bec11a3e4ef
Kiirii <k.nikolnikov@apparence.io>
2022-02-04 15:08:32 +0100

    Migration to cameraX

--- interface.dart
+++ interface.dart
@@ -1,9 +1,23 @@
 import 'package:pigeon/pigeon.dart';
 
+class PreviewSize{
+  late double width;
+  late double height;
+}
+
+class PreviewData{
+  double? textureId;
+  PreviewSize? size;
+}
+
 @HostApi()
 abstract class CameraInterface {
 
-  String getPreviewTextureId();
+  @async
+  void setupCamera();
+  List<String> checkPermissions();
+  List<String> requestPermissions();
+  PreviewData getPreviewTextureId(int cameraId);
   String takePicture();
   String takeVideo();
 }
