4eb48b531cc3c6f197744764dc26207c0bdecc00
Sébastien Bel <kobatsu.mail@gmail.com>
2022-12-16 15:45:36 +0100

    Add patrol tests, fix GPS metadata not saved on Android

---
+++ example/scripts/run_patrol.sh
@@ -0,0 +1,11 @@
+# Activate patrol cli
+#dart pub global activate patrol_cli
+
+# Run all tests
+patrol drive
+
+# Or only run specific tests
+#patrol drive \
+#  --target integration_test/ui_test.dart \
+#  --target integration_test/photo_test.dart \
+#  --target integration_test/video_test.dart
