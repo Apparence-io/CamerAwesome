25c89cca53b25b43db7e94fa60ad7e075cede28c
Dimitri Dessus <iphoneretro@icloud.com>
2022-12-28 15:55:43 +0100

    ✨ Add format & image rotation enum

---
+++ ios/Classes/Constants/InputAnalysisImageFormat.h
@@ -0,0 +1,21 @@
+//
+//  InputAnalysisImageFormat.h
+//  Pods
+//
+//  Created by Dimitri Dessus on 26/12/2022.
+//
+
+#ifndef InputAnalysisImageFormat_h
+#define InputAnalysisImageFormat_h
+
+typedef enum {
+  yuv_420_888,
+  nv21,
+  yv12,
+  jpeg,
+  yuv420,
+  bgra8888,
+  unknown
+} InputAnalysisImageFormat;
+
+#endif /* InputAnalysisImageFormat_h */
