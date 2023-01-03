bc64ed0b79314d7ba42f7531b80a5084e7fb4bef
Dimitri Dessus <iphoneretro@icloud.com>
2023-01-03 17:26:10 +0100

    💡 Add comments

--- lib/src/orchestrator/models/sensor_type.dart
+++ lib/src/orchestrator/models/sensor_type.dart
@@ -1,10 +1,18 @@
 enum SensorType {
-  // back
+  /// A built-in wide-angle camera.
+  ///
+  /// The wide angle sensor is the default sensor for iOS
   wideAngle,
+
+  /// A built-in camera with a shorter focal length than that of the wide-angle camera.
   ultraWideAngle,
+
+  /// A built-in camera device with a longer focal length than the wide-angle camera.
   telephoto,
 
-  // front
+  /// A device that consists of two cameras, one Infrared and one YUV.
+  ///
+  /// iOS only
   trueDepth,
 
   unknown;
@@ -14,9 +22,13 @@ enum SensorType {
 
 class SensorTypeDevice {
   final SensorType sensorType;
+  /// A localized device name for display in the user interface.
   final String name;
+  /// The current exposure ISO value.
   final num iso;
+  /// A Boolean value that indicates whether the flash is currently available for use.
   final bool flashAvailable;
+  /// An identifier that uniquely identifies the device.
   final String uid;
 
   SensorTypeDevice({
@@ -28,13 +40,28 @@ class SensorTypeDevice {
   });
 }
 
+// TODO: instead of storing SensorTypeDevice values,
+// this would be useful when CameraX will support multiple sensors.
+// store them in a list of SensorTypeDevice.
+// ex:
+// List<SensorTypeDevice> wideAngle;
+// List<SensorTypeDevice> ultraWideAngle;
+
 class SensorDeviceData {
-  // back
+  /// A built-in wide-angle camera.
+  ///
+  /// The wide angle sensor is the default sensor for iOS
   SensorTypeDevice? wideAngle;
+
+  /// A built-in camera with a shorter focal length than that of the wide-angle camera.
   SensorTypeDevice? ultraWideAngle;
+
+  /// A built-in camera device with a longer focal length than the wide-angle camera.
   SensorTypeDevice? telephoto;
 
-  // front
+  /// A device that consists of two cameras, one Infrared and one YUV.
+  ///
+  /// iOS only
   SensorTypeDevice? trueDepth;
 
   SensorDeviceData({
