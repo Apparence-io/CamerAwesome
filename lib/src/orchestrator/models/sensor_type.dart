52fc265db216a470764eb7eb0fbecb5145fd9bf1
Dimitri Dessus <iphoneretro@icloud.com>
2023-01-03 14:54:13 +0100

    ✨ Create sensor type device model

---
+++ lib/src/orchestrator/models/sensor_type.dart
@@ -0,0 +1,54 @@
+enum SensorType {
+  // back
+  wideAngle,
+  ultraWideAngle,
+  telephoto,
+
+  // front
+  trueDepth,
+
+  unknown;
+
+  SensorType get defaultSensorType => SensorType.wideAngle;
+}
+
+class SensorTypeDevice {
+  final SensorType sensorType;
+  final String name;
+  final num iso;
+  final bool flashAvailable;
+  final String uid;
+
+  SensorTypeDevice({
+    required this.sensorType,
+    required this.name,
+    required this.iso,
+    required this.flashAvailable,
+    required this.uid,
+  });
+}
+
+class SensorDeviceData {
+  SensorTypeDevice? wideAngle;
+  SensorTypeDevice? ultraWideAngle;
+  SensorTypeDevice? telephoto;
+
+  SensorTypeDevice? trueDepth;
+
+  SensorDeviceData({
+    this.wideAngle,
+    this.ultraWideAngle,
+    this.telephoto,
+    this.trueDepth,
+  });
+
+  int get availableBackSensors => [
+        wideAngle,
+        ultraWideAngle,
+        telephoto,
+      ].where((element) => element != null).length;
+
+  int get availableFrontSensors => [
+        trueDepth,
+      ].where((element) => element != null).length;
+}
