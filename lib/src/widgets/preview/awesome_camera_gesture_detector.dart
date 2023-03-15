e191eb85a4c4e1acbe5d49fe938dea4fb14cb08c
jeremy <j.hammerer@apparence.io>
2023-03-15 16:41:02 +0100

    update zoom

--- lib/src/widgets/preview/awesome_camera_gesture_detector.dart
+++ lib/src/widgets/preview/awesome_camera_gesture_detector.dart
@@ -66,15 +66,15 @@ class AwesomeCameraGestureDetector extends StatefulWidget {
 
 class _AwesomeCameraGestureDetector
     extends State<AwesomeCameraGestureDetector> {
-  double _previousZoomScale = 0;
   double _zoomScale = 0;
+  final double _accuracy = 0.01;
+  double? _lastScale;
+
   Offset? _tapPosition;
   Timer? _timer;
-  double? _firstOffset;
 
   @override
   void initState() {
-    _previousZoomScale = widget.initialZoom;
     _zoomScale = widget.initialZoom;
     super.initState();
   }
@@ -88,19 +88,23 @@ class _AwesomeCameraGestureDetector
               GestureRecognizerFactoryWithHandlers<ScaleGestureRecognizer>(
             () => ScaleGestureRecognizer()
               ..onStart = (_) {
-                _previousZoomScale = _zoomScale + 1;
-                _firstOffset = null;
+                _lastScale = null;
               }
               ..onUpdate = (ScaleUpdateDetails details) {
-                _firstOffset ??= details.scale - 1;
+                _lastScale ??= details.scale;
 
-                double result =
-                    _previousZoomScale * (details.scale - _firstOffset!) - 1;
-
-                if (result < 1 && result > 0) {
-                  _zoomScale = result;
-                  widget.onPreviewScale!.onScale(_zoomScale);
+                if (details.scale < (_lastScale! + 0.01) &&
+                    details.scale > (_lastScale! - 0.01)) {
+                  return;
+                } else if (_lastScale! < details.scale) {
+                  _zoomScale += _accuracy;
+                } else {
+                  _zoomScale -= _accuracy;
                 }
+
+                _zoomScale = _zoomScale.clamp(0, 1);
+                widget.onPreviewScale!.onScale(_zoomScale);
+                _lastScale = details.scale;
               },
             (instance) {},
           ),
