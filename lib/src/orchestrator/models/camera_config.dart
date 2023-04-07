7e0cdc1c731681a79d17bc71b305dc0c1d4b6d4b
Sébastien Bel <kobatsu.mail@gmail.com>
2023-04-07 14:52:59 +0200

    🔥 Remove unused class

--- lib/src/orchestrator/models/camera_config.dart
+++
@@ -1,9 +0,0 @@
-import 'sensors.dart';
-
-class CameraConfig {
-  final List<Sensor> sensors;
-
-  CameraConfig.single(Sensor sensor) : sensors = [sensor];
-
-  CameraConfig.multiple(this.sensors);
-}
