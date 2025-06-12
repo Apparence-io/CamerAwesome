55ce7f12c1ba097ce1765b14ac72eb4e0731d46c
Henrik Feldt <henrik@haf.se>
2025-06-12 12:22:08 +0200

    fix(camera): Reset digital zoom when switching sensors
    When switching between physical sensors (e.g., from wide to telephoto), the previous digital zoom level was being carried over. This resulted in an excessive zoom level (optical zoom + digital zoom). This fix resets the digital zoom to 0.0 whenever the sensor type is changed, ensuring that only the selected optical zoom is applied.

--- lib/src/orchestrator/states/camera_state.dart
+++ lib/src/orchestrator/states/camera_state.dart
@@ -81,8 +81,7 @@ abstract class CameraState {
       // new : [wide, front, telephoto]
       final newSensorsCopy = [...previous.sensors.nonNulls];
       next = SensorConfig.multiple(
-        sensors: newSensorsCopy
-          ..insert(0, newSensorsCopy.removeAt(newSensorsCopy.length - 1)),
+        sensors: newSensorsCopy..insert(0, newSensorsCopy.removeAt(newSensorsCopy.length - 1)),
         // TODO Initial values are not set in native when set like this
         aspectRatio: aspectRatio ?? CameraAspectRatios.ratio_4_3,
         zoom: zoom ?? 0.0,
@@ -127,7 +126,7 @@ abstract class CameraState {
           .toList(),
       aspectRatio: previous.aspectRatio,
       flashMode: previous.flashMode,
-      zoom: previous.zoom,
+      zoom: 0.0,
     );
     cameraContext.setSensorConfig(next);
   }
@@ -190,6 +189,5 @@ abstract class CameraState {
     return cameraContext.previewTextureId(cameraPosition);
   }
 
-  AnalysisController? get analysisController =>
-      cameraContext.analysisController;
+  AnalysisController? get analysisController => cameraContext.analysisController;
 }
